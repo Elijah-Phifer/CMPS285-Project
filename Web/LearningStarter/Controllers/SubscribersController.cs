f47ac5b465568a9e17e4e7083ee67b31ff959f49
kearneybutler <kearneybutler95@gmail.com>
2022-10-01 21:18:44 -0500

    Newsletters and subscribers

--- Web/LearningStarter/Controllers/SubscribersController.cs
+++ Web/LearningStarter/Controllers/SubscribersController.cs
@@ -8,39 +8,32 @@ using Microsoft.AspNetCore.Mvc;
 namespace LearningStarter.Controllers
 {
     [ApiController]
-    [Route("api/subscribers")]
+    [Route("api/subscriber")]
     public class SubscribersController : ControllerBase
     {
         private readonly DataContext _dataContext;
-
         public SubscribersController(
             DataContext dataContext)
         {
             _dataContext = dataContext;
-
         }
+
         [HttpGet]
         public IActionResult GetAll()
         {
             var response = new Response();
-
             var subscribers = _dataContext
             .Subscribers
-            .Select(subscribers => new SubcribersGetDto
+            .Select(subscriber => new SubscriberGetDto
             {
-                Id = subscribers.Id,
-                Name = subscribers.SubcriberId,
-                Email = subscribers.Title,
-                DateSubscribed = subscribers.DateSubscribed,
+                Id = subscriber.Id,
+                Name = subscriber.Title,
+                Email = subscriber.Message,
+                DateSubcribed = subscriber.DateSent,
             })
             .ToList();
-
             response.Data = subscribers;
             return Ok(response);
-
-
-
-
+        }
         }
     }
-}
