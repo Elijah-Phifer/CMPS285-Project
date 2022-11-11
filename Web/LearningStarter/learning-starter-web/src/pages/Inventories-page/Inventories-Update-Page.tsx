99932b735a9a8a731c029aa81c3429de04930681
Aiden Hrenyk <aiden.hrenyk@selu.edu>
2022-11-11 13:07:09 -0600

    more work on update page

--- Web/LearningStarter/learning-starter-web/src/pages/Inventories-page/Inventories-Update-Page.tsx
+++ Web/LearningStarter/learning-starter-web/src/pages/Inventories-page/Inventories-Update-Page.tsx
@@ -2,20 +2,25 @@ import axios from "axios";
 import { Field, Form, Formik } from "formik";
 import React, { useEffect, useState } from "react";
 import { Button, Input, Label } from "semantic-ui-react";
-import { ApiResponse, InventoriesGetDto } from "../../constants/types";
+import {
+  ApiResponse,
+  InventoriesCreateDto,
+  InventoriesGetDto,
+} from "../../constants/types";
 import { useRouteMatch } from "react-router-dom";
 import { BaseUrl } from "../../constants/ens-vars";
 
 export const InventoriesUpdatePage = () => {
+  let match = useRouteMatch<{ id: string }>();
+  const id = match.params.id;
+  const [inventories, setInventories] = useState<InventoriesGetDto>();
+
   const onSubmit = async (values: InventoriesGetDto) => {
     const response = await axios.post<ApiResponse<InventoriesGetDto>>(
       `${BaseUrl}/api/Inventories`,
       values
     );
   };
-  let match = useRouteMatch<{ id: string }>();
-  const id = match.params.id;
-  const [inventories, setInventories] = useState<InventoriesGetDto>();
 
   useEffect(() => {
     const fetchInvetories = async () => {
@@ -26,7 +31,7 @@ export const InventoriesUpdatePage = () => {
       setInventories(response.data.data);
     };
     fetchInvetories();
-  }, []);
+  }, [id]);
   return (
     <>
       {inventories && (
@@ -34,50 +39,51 @@ export const InventoriesUpdatePage = () => {
           <Form>
             {/* <div>
               <Label>Item Name</Label>
-            </div>
+            </div> 
+
             <Field id="itemName" name="itemName">
               {({ field }) => <Input {...field} />}
             </Field> */}
             <div>
               <label htmlFor="itemName">Item Name</label>
             </div>
-            <Field id="itemName" name="itemName">
+            <Field id="${id}" name="itemName">
               {({ field }) => <Input {...field} />}
             </Field>
             <div>
               <label htmlFor="productionCost">production Cost</label>
             </div>
-            <Field id="productionCost" name="productionCost">
+            <Field id="${id}" name="productionCost">
               {({ field }) => <Input {...field} />}
             </Field>
             <div>
               <label htmlFor="quantity">Quantity</label>
             </div>
-            <Field id="quantity" name="quantity">
+            <Field id="${id}" name="quantity">
               {({ field }) => <Input {...field} />}
             </Field>
             <div>
               <label htmlFor="availabilty">availabilty</label>
             </div>
-            <Field id="availabilty" name="availabilty">
+            <Field id="${id}" name="availabilty">
               {({ field }) => <Input {...field} />}
             </Field>
             <div>
               <label htmlFor="onlineStoreId">onlineStoreId</label>
             </div>
-            <Field id="onlineStoreId" name="onlineStoreId">
+            <Field id="${id}" name="onlineStoreId">
               {({ field }) => <Input {...field} />}
             </Field>
             <div>
               <label htmlFor="siteListing">Site Listing</label>
             </div>
-            <Field id="siteListing" name="siteListing">
+            <Field id="${id}" name="siteListing">
               {({ field }) => <Input {...field} />}
             </Field>
             <div>
               <label htmlFor="dateAdded">Date Added</label>
             </div>
-            <Field id="dateAdded" name="dateAdded">
+            <Field id="${id}" name="dateAdded">
               {({ field }) => <Input {...field} />}
             </Field>
             <div>
