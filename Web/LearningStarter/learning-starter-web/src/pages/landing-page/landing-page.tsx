3c7df78dce1bda236888d23c8300fa0a69036b40
Elijah Phifer <elijah.phifer@selu.edu>
2022-11-25 17:00:36 -0600

    testing out cards

--- Web/LearningStarter/learning-starter-web/src/pages/landing-page/landing-page.tsx
+++ Web/LearningStarter/learning-starter-web/src/pages/landing-page/landing-page.tsx
@@ -1,13 +1,34 @@
-import React from "react";
-import { Button, Header } from "semantic-ui-react";
+import React, { useState } from "react";
+import { Button, Card, Header, Icon } from "semantic-ui-react";
 import { useHistory } from "react-router-dom";
 import "./landing-page.css";
 import { routes } from "../../routes/config";
+import { BaseUrl } from "../../constants/ens-vars";
+import { ApiResponse, BulletJournalEntryGetDTO } from "../../constants/types";
+import axios from "axios";
 
 //This is a basic Component, and since it is used inside of
 //'../../routes/config.tsx' line 31, that also makes it a page
 export const LandingPage = () => {
   const history = useHistory();
+  const [bulletJournalEntries, setBulletJournalEntries] =
+    useState<BulletJournalEntryGetDTO[]>();
+
+  const fetchBulletJournal = async () => {
+    const response = await axios.get<ApiResponse<BulletJournalEntryGetDTO[]>>(
+      `${BaseUrl}/api/BulletJournal`
+    );
+    if (response.data.hasErrors) {
+      alert("Something went wrong!");
+      return;
+      /*response.data.errors.forEach(err => {
+                console.log(err.message);
+            });*/
+    } else {
+      setBulletJournalEntries(response.data.data);
+    }
+  };
+
   return (
     <div className="background">
       <div className="home-page-container">
@@ -47,3 +68,24 @@ export const LandingPage = () => {
     </div>
   );
 };
+
+/*export const CardExampleCard = () => (
+  <Card>
+    <Card.Content>
+      <Card.Header>Matthew</Card.Header>
+      <Card.Meta>
+        <span className="date">Joined in 2015</span>
+      </Card.Meta>
+      <Card.Description>
+        Matthew is a musician living in Nashville.
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name="user" />
+        22 Friends
+      </a>
+    </Card.Content>
+  </Card>
+);
+*/
